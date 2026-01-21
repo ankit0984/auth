569abdbda5445ba7dcd6c345d281338e78abc656
ankitkumar <ak1410mk@gmail.com>
2026-01-21 18:19:44 +0530

    update db conn

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,11 @@
 import app from "./app.js";
 import dbconfig from "../db/db.js";
-import redisClient from "../db/redis.js";
 import dotenv from "dotenv";
 
 dotenv.config({path: "../.env"});
 dbconfig()
   .then(() => {
-    redisClient;
+    // redisClient;
     app.listen(process.env.SERVER_PORT || 3636, () => {
       console.log(`server is online on port => ${process.env.SERVER_PORT}`);
     });
